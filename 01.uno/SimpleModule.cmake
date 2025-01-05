a7cb02e7de1fed07826292280bdca49c3a241b3c
Anton Tagunov <15095198+atagunov@users.noreply.github.com>
2025-01-05 02:40:15 +0000

    Adding GTest test

--- 01.uno/SimpleModule.cmake
+++ 01.uno/SimpleModule.cmake
@@ -1,5 +1,5 @@
 # This project is using a "pseudo-module" facility
-# actual C++20 modules are perceived as not being mature enough for prod use at the moment
+# Actual C++20 modules are perceived as not being mature enough for prod use at the moment
 #
 # "simple_module" function facilitates building this "pseudo-module" hierarchy
 # Each src/**/*.cc file is considered a "simple_module"
@@ -12,17 +12,22 @@
 # 1st it creates a "library" which contains only dependencies of our "simple module" file - other .cc files
 # 2nd it creates a "library" which contains all of the above plus the .cc file itself
 #
-# So we get for example "uno-util-dependencies" and "uno-util" OBJECT libraries
+# So we get for example "uno-util-dependencies" and "uno-util" cmake targets
+# Each is just a collection of .o files
 #
-# The former is used when compiling -test.cc for the source file
-# We need say it to be separate from "uno-util"
-# because "uno/util-test.cc" actually _includes_ "uno/util.cc" in order to do white-box testing
+# We use "uno-util-dependencies" when compiling "uno/util-test.cc"
+# We do it this way because "uno/util-test.cc" actually _includes_ "uno/util.cc" in order to do white-box testing
 #
-# The bigger "uno-util" is used when higher-level modules like "uno" are declared
-# Each higher-level module includes .o files for all subordinate modules into its targets
+# The bigger "uno-util" is used when higher-level "modules" like "uno" are composed
+# Each higher-level module includes .o files for all subordinate modules
+# For example "uno" target includes both "uno.o" and "uno/util.o" as well as any other .o files which "util.o" may need
 #
-# Additionally this function adds adds global-includes as a private dependency to both of
+# Additionally "simple_module" function adds adds global-includes as a private dependency to both of
 # the "library" targets it creates so that we use correct -I when compiling
+#
+# For an executable "global-includes" just specifies to use "src" folder for "-I"
+# If we were building a library we could have included both "src" (for our private headers)
+# and a separate "include" folder which would then contain the public API of our library
 
 function(simple_module)
     if(${CMAKE_CURRENT_SOURCE_DIR} STREQUAL "${PROJECT_SOURCE_DIR}/src")
